f672eb29dc5bda7fa40211483d70360b310746b9
Vaniaina <vanimendes344@gmail.com>
2026-05-27 17:06:19 +0300

    fix: indentations

--- src/main/java/test/poja/first/endpoint/rest/controller/CalcController.java
+++ src/main/java/test/poja/first/endpoint/rest/controller/CalcController.java
@@ -11,29 +11,29 @@ import test.poja.first.service.CalcService;
 @RestController
 public class CalcController {
 
-    private final CalcService calcService;
+  private final CalcService calcService;
 
-    public CalcController(CalcService calcService) {
-        this.calcService = calcService;
-    }
+  public CalcController(CalcService calcService) {
+    this.calcService = calcService;
+  }
 
-    @GetMapping("/add")
-    public ResponseEntity<Integer> add(@RequestParam int a, @RequestParam int b) {
-        return ResponseEntity.ok(calcService.add(a, b));
-    }
+  @GetMapping("/add")
+  public ResponseEntity<Integer> add(@RequestParam int a, @RequestParam int b) {
+    return ResponseEntity.ok(calcService.add(a, b));
+  }
 
-    @GetMapping("/subtract")
-    public ResponseEntity<Integer> subtract(@RequestParam int a, @RequestParam int b) {
-        return ResponseEntity.ok(calcService.subtract(a, b));
-    }
+  @GetMapping("/subtract")
+  public ResponseEntity<Integer> subtract(@RequestParam int a, @RequestParam int b) {
+    return ResponseEntity.ok(calcService.subtract(a, b));
+  }
 
-    @GetMapping("/multiply")
-    public ResponseEntity<Integer> multiply(@RequestParam int a, @RequestParam int b) {
-        return ResponseEntity.ok(calcService.multiply(a, b));
-    }
+  @GetMapping("/multiply")
+  public ResponseEntity<Integer> multiply(@RequestParam int a, @RequestParam int b) {
+    return ResponseEntity.ok(calcService.multiply(a, b));
+  }
 
-    @GetMapping("/divide")
-    public ResponseEntity<Double> divide(@RequestParam int a, @RequestParam int b) {
-        return ResponseEntity.ok(calcService.divide(a, b));
-    }
+  @GetMapping("/divide")
+  public ResponseEntity<Double> divide(@RequestParam int a, @RequestParam int b) {
+    return ResponseEntity.ok(calcService.divide(a, b));
+  }
 }
