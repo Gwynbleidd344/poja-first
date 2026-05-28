5c354a599d5c6645d6ed447252ef4764fc7f2345
unknown <vanimendes344@gmail.com>
2026-05-28 14:58:36 +0300

    Fix: formatage + add test

--- src/main/java/test/poja/first/endpoint/rest/controller/calculus/CalcController.java
+++ src/main/java/test/poja/first/endpoint/rest/controller/calculus/CalcController.java
@@ -1,9 +1,7 @@
-package test.poja.first.rest.controller;
+package test.poja.first.endpoint.rest.controller.calculus;
 
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import test.poja.first.service.CalcService;
@@ -18,22 +16,22 @@ public class CalcController {
   }
 
   @GetMapping("/add")
-  public ResponseEntity<Integer> add(@RequestParam int a, @RequestParam int b) {
+  public ResponseEntity<Integer> add(@RequestParam("a") int a, @RequestParam("b") int b) {
     return ResponseEntity.ok(calcService.add(a, b));
   }
 
   @GetMapping("/subtract")
-  public ResponseEntity<Integer> subtract(@RequestParam int a, @RequestParam int b) {
+  public ResponseEntity<Integer> subtract(@RequestParam("a") int a, @RequestParam("b") int b) {
     return ResponseEntity.ok(calcService.subtract(a, b));
   }
 
   @GetMapping("/multiply")
-  public ResponseEntity<Integer> multiply(@RequestParam int a, @RequestParam int b) {
+  public ResponseEntity<Integer> multiply(@RequestParam("a") int a, @RequestParam("b") int b) {
     return ResponseEntity.ok(calcService.multiply(a, b));
   }
 
   @GetMapping("/divide")
-  public ResponseEntity<Double> divide(@RequestParam int a, @RequestParam int b) {
+  public ResponseEntity<Double> divide(@RequestParam("a") int a, @RequestParam("b") int b) {
     return ResponseEntity.ok(calcService.divide(a, b));
   }
 }
--- src/main/java/test/poja/first/service/CalcService.java
+++ src/main/java/test/poja/first/service/CalcService.java
@@ -1,6 +1,7 @@
 package test.poja.first.service;
 
 import org.springframework.stereotype.Service;
+import java.lang.ArithmeticException;
 
 @Service
 public class CalcService {
@@ -33,11 +34,11 @@ public class CalcService {
   private void checkOverflow(long result) {
     if (result > Integer.MAX_VALUE) {
       throw new ArithmeticException(
-          "Result exceeds Integer.MAX_VALUE (" + Integer.MAX_VALUE + "): " + result);
+        "Result exceeds Integer.MAX_VALUE (" + Integer.MAX_VALUE + "): " + result);
     }
     if (result < Integer.MIN_VALUE) {
       throw new ArithmeticException(
-          "Result is below Integer.MIN_VALUE (" + Integer.MIN_VALUE + "): " + result);
+        "Result is below Integer.MIN_VALUE (" + Integer.MIN_VALUE + "): " + result);
     }
   }
 }
